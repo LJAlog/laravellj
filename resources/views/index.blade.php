c92dd02f77fb40077432fae41ab8ebd7ca0bcdf4
LJ Alog <alog.lawrencejustine@gmail.com>
2024-07-18 16:21:19 +0800

    Laravel Attempt

---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+        <h1>LARAVEL TEST v.1</h1>
+
+        <a href="{{url ('/post') }}">POST </a>
+    </body>
+</html>
---
+++ resources/views/post/create.blade.php
@@ -0,0 +1,18 @@
+<form action="">
+<div>
+<p>Title:</p>
+<input type="text" names="title" id="title">
+</div>
+
+<div>
+<p></p>
+
+</div>
+
+
+
+
+
+
+
+</form>
